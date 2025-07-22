43f3cb8df670cc909b2196bd8bd4aa6e6254950e
francescoboschelle <francibossch@gmail.com>
2025-07-22 09:51:32 +0200

    Refactor Main component to implement genre filtering and search functionality

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,6 +4,7 @@ import movies from "../data/data";
 export default function Main() {
   const [filter, setFilter] = useState("All");
   const [data, setData] = useState([movies]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     let newData;
@@ -13,8 +14,12 @@ export default function Main() {
       newData = movies.filter((movie) => movie.genre === filter);
     }
 
+    if (query !== "") {
+      newData = newData.filter((movie) => movie.title.startsWith(query));
+    }
+
     setData(newData);
-  }, [filter]);
+  }, [filter, query]);
 
   return (
     <main>
@@ -23,6 +28,53 @@ export default function Main() {
           <div className="card-body">
             <h1 className="card-title text-center">Lista film</h1>
             <hr />
+
+            <div className="d-flex gap-3">
+              <div className="dropdown mb-3">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="triggerId"
+                  data-bs-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  {filter === "All" ? "Filter by Genres" : filter}
+                </button>
+                <div className="dropdown-menu" aria-labelledby="triggerId">
+                  <p
+                    className="dropdown-item"
+                    key={`dropdown:0`}
+                    onClick={() => setFilter("All")}
+                  >
+                    Filter by Genres
+                  </p>
+                  {movies.map((movie, index) => {
+                    return (
+                      <p
+                        className="dropdown-item"
+                        key={`dropdown:${index}`}
+                        onClick={() => setFilter(movie.genre)}
+                      >
+                        {movie.genre}
+                      </p>
+                    );
+                  })}
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  name=""
+                  id=""
+                  aria-describedby="helpId"
+                  placeholder="Search movie"
+                  onChange={(e) => setQuery(e.target.value)}
+                />
+              </div>
+            </div>
 
             <ul className="list-group">
               {data.map((movie, index) => {
@@ -33,39 +85,6 @@ export default function Main() {
                 );
               })}
             </ul>
-
-            <div className="dropdown mt-3">
-              <button
-                className="btn btn-secondary dropdown-toggle"
-                type="button"
-                id="triggerId"
-                data-bs-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                {filter}
-              </button>
-              <div className="dropdown-menu" aria-labelledby="triggerId">
-                <p
-                  className="dropdown-item"
-                  key={`dropdown:0`}
-                  onClick={() => setFilter("All")}
-                >
-                  All
-                </p>
-                {movies.map((movie, index) => {
-                  return (
-                    <p
-                      className="dropdown-item"
-                      key={`dropdown:${index}`}
-                      onClick={() => setFilter(movie.genre)}
-                    >
-                      {movie.genre}
-                    </p>
-                  );
-                })}
-              </div>
-            </div>
           </div>
         </div>
       </div>
